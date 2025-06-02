b1949782b9896be89c7503ae14b38d0c9fd5e239
kimduyminh <minhkimfp212@gmail.com>
2025-06-02 11:38:25 +0700

    add seed saodon

---
+++ backend/seed_sao_don.sql
@@ -0,0 +1,17 @@
+INSERT INTO user_profiles (
+    id, email, role, created_at, last_login
+) VALUES (
+    'bf52e8fd-3f84-4f5e-a256-db811adc8b7c',
+    'student01@example.com',
+    'student',
+    strftime('%s', 'now'),
+    strftime('%s', 'now')
+);
+
+-- Thêm profile học sinh tương ứng vào student_profiles
+INSERT INTO student_profiles (
+    id, language_level, points, money, hp, atk, items
+) VALUES (
+    'bf52e8fd-3f84-4f5e-a256-db811adc8b7c',
+    1, 0, 100, 100, 10, '[]'
+);
