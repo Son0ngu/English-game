5e55d409f7df953ce35f82542862fb2e0b49c947
kimduyminh <minhkimfp212@gmail.com>
2025-11-30 19:58:01 +0700

    feat: add auth_service database interface, signup service

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,29 @@
+import os
+from datetime import timedelta
+import dotenv
+from flask_jwt_extended import JWTManager
 from flask import Flask
 from api_gateway.gateway_service import gateway_service
 app = Flask(__name__)
 
+# Load environment variables from .env file
+dotenv.load_dotenv()
 
+# JWT Configurations
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=120)
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+app.config['JWT_TOKEN_LOCATION'] = ['headers','cookies']
+app.config['JWT_COOKIE_SECURE'] = True
+app.config['JWT_COOKIE_SAMESITE'] = 'LAX'
+app.config['JWT_COOKIE_CSRF_PROTECT'] = True
+app.config['JWT_ACCESS_COOKIE_PATH'] = '/'
+app.config['JWT_REFRESH_COOKIE_PATH'] = '/'
+jwt = JWTManager(app)
+# Service instances:
+gateway_service = gateway_service(app)
 @app.route('/', defaults={'path': ''}, methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
 @app.route('/<path:path>', methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
+
 def request(path):
     gateway_service.new_request(path)
 
---
+++ backend/auth_service/login_and_register_service/login_service.py
@@ -0,0 +1,7 @@
+from auth_service.login_and_register_service.auth_service_database_interface import auth_service_database_interface
+
+class login_service:
+    def __init__(self):
+        self.auth_service_database_interface = auth_service_database_interface
+        pass
+    def login(self, username, password):
