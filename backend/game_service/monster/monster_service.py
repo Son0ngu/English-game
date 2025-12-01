a3996560b2be37c858b3c548c423e6a50b9cdc8b
kimduyminh <minhkimfp212@gmail.com>
2025-12-01 19:47:22 +0700

    rev: remove add question in game service ref: refactor difficulty and question type format fix: change user_id source, now taken from jwt

--- backend/game_service/monster/monster_service.py
+++ backend/game_service/monster/monster_service.py
@@ -9,22 +9,22 @@ class monster_service:
         student_hp = user.get_hp(student_id)
         student_atk = user.get_atk(student_id)
 
-        if student_difficulty == "easy":
+        if student_difficulty == 1:
             self.monster.monster_hp = student_hp * 1.05
             self.monster.monster_atk = student_atk * 1.05
             self.monster.money_win = 10
 
-        elif student_difficulty == "medium":
+        elif student_difficulty == 2:
             self.monster.monster_hp = student_hp * 1.5
             self.monster.monster_atk = student_atk * 1.5
             self.monster.money_win = 20
 
-        elif student_difficulty == "hard":
+        elif student_difficulty == 3:
             self.monster.monster_hp = student_hp * 2.2
             self.monster.monster_atk = student_atk * 2.2
             self.monster.money_win = 50
 
-        elif student_difficulty == "give_me_god_of_war":
+        elif student_difficulty == 4:
             self.monster.monster_hp = student_hp * 10
             self.monster.monster_atk = student_atk * 10
             self.monster.money_win = 150
