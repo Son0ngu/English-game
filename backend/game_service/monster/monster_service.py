ca5c1d06ea0f4e922a890a0a9fa52481d3a4004a
kimduyminh <minhkimfp212@gmail.com>
2025-12-02 12:56:56 +0700

    fix: fix difficulty datatype in monster creation

--- backend/game_service/monster/monster_service.py
+++ backend/game_service/monster/monster_service.py
@@ -4,25 +4,28 @@ class monster_service:
         pass
 
     def create_monster_based_on_difficulty(self,student_id,student_difficulty,student_hp,student_atk):
+        print("stop")
         new_monster = monster.monster()
-        if student_difficulty == 1:
+        if student_difficulty == "easy":
             new_monster.monster_hp = student_hp * 1.05
             new_monster.monster_atk = student_atk * 1.05
             new_monster.money_win = 10
 
-        elif student_difficulty == 2:
+        elif student_difficulty == "medium":
             new_monster.monster_hp = student_hp * 1.5
             new_monster.monster_atk = student_atk * 1.5
             new_monster.money_win = 20
 
-        elif student_difficulty == 3:
+        elif student_difficulty == "hard":
             new_monster.monster_hp = student_hp * 2.2
             new_monster.monster_atk = student_atk * 2.2
             new_monster.money_win = 50
 
-        elif student_difficulty == 4:
+        elif student_difficulty == "very hard":
             new_monster.monster_hp = student_hp * 10
             new_monster.monster_atk = student_atk * 10
             new_monster.money_win = 150
-
+        print(new_monster.monster_hp)
+        print(new_monster.monster_atk)
+        print(new_monster.money_win)
         return new_monster
